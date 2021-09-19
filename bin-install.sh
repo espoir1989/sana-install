dffe9d981805ffa595196d505625452f97d999c8
espoir <yghack@gmail.com>
2021-09-20 00:01:04 +0800

    add github speed up

--- bin-install.sh
+++ bin-install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 apt update 
 apt install zip unzip curl wget nginx screen vim -y
-wget https://github.com/ethsana/sana/releases/download/v0.1.2/ant-linux-amd64
-wget https://github.com/ethsana/exportSanaKey/releases/download/v0.1.0/export-sana-key-linux-amd64
+wget https://github.91chifun.workers.dev/https://github.com/ethsana/sana/releases/download/v0.1.2/ant-linux-amd64
+wget https://github.91chifun.workers.dev/https://github.com/ethsana/exportSanaKey/releases/download/v0.1.0/export-sana-key-linux-amd64
 mv ant-linux-amd64 /usr/bin/ant
 mv export-sana-key-linux-amd64 /usr/bin/ExportKey
 chmod -R 777 /usr/bin/ant /usr/bin/ExportKey
 mkdir /home/ant
 wget https://raw.githubusercontent.com/espoir1989/sana-install/main/ant.yaml
-wget https://github.com/ethsana/ant-dashboard/archive/refs/heads/online.zip
+wget https://github.91chifun.workers.dev/https://github.com/ethsana/ant-dashboard/archive/refs/heads/online.zip
 unzip online.zip
 rm -rf /var/www/html/*
 cp -r ant-dashboard-online/* /var/www/html/
